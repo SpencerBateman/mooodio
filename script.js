472bc5a9599023cd692b400b7a2995f73dfde96e
Spencer Bateman <bateman.s@husky.neu.edu>
2017-11-01 19:08:00 -0400

    test

---
+++ script.js
@@ -0,0 +1,32 @@
+$(function() {
+  $(".card").draggable();
+
+  $('.input_field').keydown(function(event) {
+    if (event.keyCode == 13) {
+      var model=$(this).val();
+      $(this).parent().css('background-image', 'url(' + model + ')');
+      $(this).css('display', 'none');
+    }
+  });
+
+  $(".plus").click(function(){
+    makeCard();
+  });
+
+  function makeCard() {
+
+
+    // Your existing code unmodified...
+    var iDiv = document.createElement('div');
+    iDiv.className = 'card';
+    document.getElementsByTagName('body')[0].appendChild(iDiv);
+
+    // Now create and append to iDiv
+    var innerDiv = document.createElement('input');
+    innerDiv.className = 'input_field';
+
+    // The variable iDiv is still good... Just append to it.
+    iDiv.appendChild(innerDiv);
+  };
+
+});
