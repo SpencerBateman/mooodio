e9151f6d4972dfa0b91c8466b45255b856b48848
Spencer Bateman <bateman.s@husky.neu.edu>
2017-11-02 18:29:06 -0400

    multiple images and cards working

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 $(function() {
   $(".card").draggable();
 
-  $('.input_field').keydown(function(event) {
-    if (event.keyCode == 13) {
-      var model=$(this).val();
-      $(this).parent().css('background-image', 'url(' + model + ')');
-      $(this).css('display', 'none');
-    }
-  });
+  addEvent($('.input_field'));
 
   $(".plus").click(function(){
     makeCard();
   });
+
+  function addEvent(target) {
+    console.log(target);
+    target.keydown(function(event) {
+      if (event.keyCode == 13) {
+        var model=$(this).val();
+        console.log(model);
+        $(this).parent().css('background-image', 'url(' + model + ')');
+        $(this).css('display', 'none');
+      }
+    });
+  }
 
   function makeCard() {
 
@@ -24,10 +30,13 @@ $(function() {
     // Now create and append to iDiv
     var innerDiv = document.createElement('input');
     innerDiv.className = 'input_field';
+    //console.log(innerDiv);
+    //addEvent(innerDiv);
 
     // The variable iDiv is still good... Just append to it.
     iDiv.appendChild(innerDiv);
     $(".card").draggable();
+    addEvent($('.input_field'));
   };
 
 });
