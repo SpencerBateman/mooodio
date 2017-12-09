d7c8a7947affa9433d51cbbc803d4e166ec47bee
zackr <reiter.z@husky.neu.edu>
2017-12-09 18:34:24 -0500

    client side board service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { AuthenticationService } from './services/authentication.service.client'
 import { SharedService } from './services/shared.service.client';
 import { BoardComponent } from './components/board/board.component';
 import { FlickrService } from './services/flickr.service.client';
+import {BoardService} from './services/board/board.service';
 
 
 @NgModule({
@@ -32,7 +33,13 @@ import { FlickrService } from './services/flickr.service.client';
     Routing
   ],
   // Client Side services here
-  providers: [ UserService, SharedService, AuthenticationService, FlickrService],
+  providers: [
+      UserService,
+      SharedService,
+      AuthenticationService,
+      FlickrService,
+      BoardService
+  ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
