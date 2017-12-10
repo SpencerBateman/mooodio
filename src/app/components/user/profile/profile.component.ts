b6601176eccf4ad97eea523e5242b113e671212d
zackr <reiter.z@husky.neu.edu>
2017-12-09 22:03:01 -0500

    switched to zacks profile

--- src/app/components/user/profile/profile.component.ts
+++ src/app/components/user/profile/profile.component.ts
@@ -1,55 +1,53 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { ActivatedRoute } from '@angular/router';
-import { UserService } from '../../../services/user.service.client';
-import { SharedService } from '../../../services/shared.service.client';
+import {Component, OnInit} from '@angular/core';
+import {UserService} from "../../../services/user.service.client";
+import {ActivatedRoute, Router} from "@angular/router";
+import {SharedService} from "../../../services/shared.service";
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-
 export class ProfileComponent implements OnInit {
-
+  // properties
   userId: string;
-  user: any;
-  username: string;
-  firstName: string;
-  lastName: string;
-  errorFlag: boolean;
+  user = {};
 
-  constructor(private sharedService: SharedService, private userService: UserService, private activatedRoute: ActivatedRoute, private router: Router) { }
+  constructor(
+    private userService: UserService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private sharedService: SharedService) { }
 
   ngOnInit() {
 
-    this.activatedRoute.params.subscribe((params: any) => {
-      this.user = this.sharedService.user || {};
-      this.userId = this.user._id;
-      this.username = this.user['username'];
-      this.firstName = this.user['firstName'];
-      this.lastName = this.user['lastName'];
-    });
+    this.activatedRoute.params
+      .subscribe(
+        (params: any) => {
+          this.user = this.sharedService.user || {};
+          this.userId = this.user['_id'];
+        }
+      );
   }
 
-  updateProfile() {
-    if (this.username != null && this.firstName != null && this.lastName != null && this.username != '' && this.firstName != '' && this.lastName != '') {
-      this.user.username = this.username;
-      this.user.firstName = this.firstName;
-      this.user.lastName = this.lastName;
-      this.userService.updateUser(this.userId, this.user).subscribe((users: any) => {
-        console.log(users);
-      });
-      this.userService.updateUser(this.userId, this.user);
-      this.router.navigate(['/profile']);
-    } else {
-      this.errorFlag = true;
-    }
+  updateProfile(navigate: boolean) {
+    this.userService.updateUser(this.userId, this.user).subscribe(
+      (data: any) => {
+        if (navigate) {
+          this.router.navigate(['/user/' + data._id + '/website']);
+        }
+      }
+    );
   }
 
   logout() {
-    this.userService.logout().subscribe((status) => {
-      this.router.navigate(['/login']);
-    });
+    this.userService.logout()
+      .subscribe(
+        (data: any) => {
+          this.router.navigate(['/login']);
+        }
+      );
   }
+
 }
+
