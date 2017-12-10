4c64962fe7a228a85daaf79fba418759a7ad01a2
zackr <reiter.z@husky.neu.edu>
2017-12-09 21:19:05 -0500

    updated schemas to handle following people

--- server/model/board/board.schema.server.js
+++ server/model/board/board.schema.server.js
@@ -1,11 +1,11 @@
 var mongoose = require('mongoose');
 var BoardSchema = mongoose.Schema ({
-    name: String,
-    _user: {type: mongoose.Schema.Types.ObjectId, ref: 'UserModel'},
-    collaborators: [{type: mongoose.Schema.Types.ObjectId, ref: 'UserModel'}],
-    images: [{type: mongoose.Schema.Types.ObjectId, ref: 'ImageModel'}],
-    //comments: [{type: mongoose.Schema.Types.ObjectId, ref: 'CommentModel'}],
-    dateCreated: { type: Date, default: Date.now }
+  name: String,
+  _user: {type: mongoose.Schema.Types.ObjectId, ref: 'UserModel'},
+  collaborators: [{type: mongoose.Schema.Types.ObjectId, ref: 'UserModel'}],
+  images: [{type: mongoose.Schema.Types.ObjectId, ref: 'ImageModel'}],
+  //comments: [{type: mongoose.Schema.Types.ObjectId, ref: 'CommentModel'}],
+  dateCreated: { type: Date, default: Date.now }
 }, {collection: 'board'});
 
 module.exports = BoardSchema;
--- server/model/image/image.schema.server.js
+++ server/model/image/image.schema.server.js
@@ -1,11 +1,12 @@
 var mongoose = require('mongoose');
 var ImageSchema = mongoose.Schema ({
-    _board: {type: mongoose.Schema.Types.ObjectId, ref: 'BoardModel'},
-    left: Number,
-    top: Number,
-    url: String,
-    length: Number,
-    width: Number
+  _board: {type: mongoose.Schema.Types.ObjectId, ref: 'BoardModel'},
+  left: Number,
+  top: Number,
+  url: String,
+  length: Number,
+  width: Number,
+  dateCreated: { type: Date, default: Date.now }
 }, {collection: 'image'});
 
 module.exports = ImageSchema;
