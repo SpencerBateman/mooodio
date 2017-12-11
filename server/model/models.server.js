d83c8bc9cc751bf4d0737683dd592df38518a09c
zackr <reiter.z@husky.neu.edu>
2017-12-11 00:42:27 -0500

    made app use connection string to connect to mlab

--- server/model/models.server.js
+++ server/model/models.server.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
+let connectionString = 'mongodb://localhost/cs5610'; // for local
 if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
   let username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
   let password = process.env.MLAB_PASSWORD_WEBDEV;
   connectionString = 'mongodb://' + username + ':' + password;
   connectionString += '@ds137206.mlab.com:37206/heroku_blxd9bxr'; // use yours
 }
-const db = mongoose.connect('mongodb://127.0.0.1:27017/mooodio', { useMongoClient: true});
+const db = mongoose.connect(connectionString, { useMongoClient: true});
 module.exports = db;
