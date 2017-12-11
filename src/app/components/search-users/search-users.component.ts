ff05f94b08ec94b9f4477f35e4d89902973ce479
zackr <reiter.z@husky.neu.edu>
2017-12-10 22:53:47 -0500

    adding component

---
+++ src/app/components/search-users/search-users.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import {UserService} from '../../services/user.service.client';
+import {ActivatedRoute} from '@angular/router';
+import {SharedService} from '../../services/shared.service.client';
+
+@Component({
+  selector: 'app-search-users',
+  templateUrl: './search-users.component.html',
+  styleUrls: ['./search-users.component.css']
+})
+export class SearchUsersComponent implements OnInit {
+
+  user: {};
+  users: [{}];
+  searchTerm: string;
+
+  constructor(
+    private userService: UserService,
+    private sharedService: SharedService) { }
+
+  ngOnInit() {
+    this.user = this.sharedService.user || {};
+  }
+
+  searchUsers() {
+    this.userService.searchUsers(this.searchTerm).subscribe((res: any) => {
+      this.users = res;
+    });
+  }
+
+}
