3aa35247542cc9e95903989e51d13848bac4f9a8
zackr <reiter.z@husky.neu.edu>
2018-02-10 22:53:22 -0500

    womp

--- server/services/user.service.server.js
+++ server/services/user.service.server.js
@@ -80,7 +80,7 @@ module.exports = function(app) {
 
   function slackBotButton(req, res) {
     console.log(req.body.payload);
-    console.log(req.body.payload.actions);
+    console.log(req.body.payload['actions']);
     const value = req.body.payload.actions[0].value;
     answerMap[value] = answerMap[value] + 1;
     let responseJson = {
